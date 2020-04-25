3314721774734b71b5014fe95f0476ec6171c0b5
Ulugbek Aripov <ulugbeks@gmail.com>
2020-04-25 19:58:51 -0400

    Added interactive LiveComponent

--- lib/publicEvents_web/live/test_component.ex
+++ lib/publicEvents_web/live/test_component.ex
@@ -1,9 +1,17 @@
 defmodule TestComponent do
   use Phoenix.LiveComponent
 
+  def handle_event("say_hello", _value, socket) do
+    val = socket.assigns.content == "ABC1234" && "ABCtest" || "ABC1234"
+    {:noreply, assign(socket, content: val)}
+  end
+
   def render(assigns) do
     ~L"""
     <div class="hero">a-<%= @content %></div>
+    <a href="#" phx-click="say_hello" phx-target="<%= @myself %>">
+      Say hello!
+    </a>
     """
   end
 end
