c06cf7f941b027c6b9077a20d6278fadc2463c85
Ulugbek Aripov <ulugbeks@gmail.com>
2020-04-29 13:50:18 -0400

    Put token to Phx session and redirect

--- lib/publicEvents_web/controllers/users/fed_auth_controller.ex
+++ lib/publicEvents_web/controllers/users/fed_auth_controller.ex
@@ -5,13 +5,27 @@ defmodule PublicEventsWeb.Users.FedAuthController do
   # alias PublicEvents.Accounts
   # alias PublicEvents.Accounts.User
 
-  def callback(conn, params) do
-    IO.inspect "+++++++ conn.assigns +++++"
-    IO.inspect conn.assigns
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider}) do
+    user_params = provider == "github" &&
+    %{
+      email: auth.info.email,
+      first_name: auth.info.first_name,
+      last_name: auth.info.last_name,
+      nickname: auth.info.nickname,
+      token: auth.credentials.token
+    } ||
+    %{email: "abc"}
+    IO.inspect "+++++++ auth +++++"
+    IO.inspect auth
     IO.inspect "+++++++ params +++++"
-    IO.inspect params
+    IO.inspect user_params
+    IO.inspect "+++++++ provider +++++"
+    IO.inspect provider
     IO.inspect "+++++++  +++++"
-
+    conn
+    |> put_flash(:info, "Welcome!")
+    |> put_session(:token, user_params.token)
+    |> redirect(to: Routes.users_user_path(conn, :index))
   end
 
 end
